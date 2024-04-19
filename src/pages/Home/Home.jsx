a9ecec50f09d4b508b2e1d714f98390d1e0cca73
Leonardo Espinosa <leonardoespinosa@MacBook-Air-de-Leonardo.local>
2024-04-19 12:38:55 -0600

    top

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,29 @@
 
 export default function Home() {
+
     return (
         <>
             <section className='top'>
-                <div className="favorite"></div>
+                <div className="favorite">
+                    <section>
+                        <p>Gasto compartido con: </p>
+                        <i>L</i>
+                        <i>J</i>
+                        <i>D</i>
+                    </section>
+                    <section className="favorite_total">
+                        <p>Total gastado <span>$2300</span></p>
+                    </section>
+                    <section className="favorite_fecha">
+                        <section>
+                            <button className="favorite_details">Ver detalles</button>
+                        </section>
+                        <section>
+                            <p>Fecha de corte: </p>
+                            <p>20 de marzo</p>
+                        </section>
+                    </section>
+                </div>
                 <div className="favorite"></div>
                 <div className="favorite"></div>
                 <div className="favorite favorite-selected"></div>
